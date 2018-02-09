5f85048ab900618091f294be2634242a90a8f792
Sedrick Levesque <sedrickidd@hotmail.com>
2018-02-09 15:41:24 -0500

    Update de code

--- Jeu.js
+++ Jeu.js
@@ -1,55 +1,92 @@
 (function()
-{
-var dessin = document.getElementById("dessin");
-    
+ {
+    var dessin = document.getElementById("dessin");
 
+
+
+    //var ennemi = new Ennemi(dessin);  
+    var scene = new createjs.Stage(dessin);   
     
-//var ennemi = new Ennemi(dessin);  
-var scene = new createjs.Stage(dessin);   
-createjs.Ticker.addEventListener("tick", rafraichirJeu);
-createjs.Ticker.setInterval(0);
-createjs.Ticker.setFPS(10);
-     function rafraichirJeu(evenement)
+    createjs.Ticker.setFPS(30);
+    function rafraichirJeu(evenement)
     {
-        scene.update();
+        var deplacementActuel = evenement.delta / 1000 * 200; 
+        hero.deplacer(deplacementActuel);
+        scene.update(evenement);
     }    
 
-var hero = new PersonnagePrincipal(dessin);    
-scene.addChild(hero.animationBlop);
-hero.jouerAnimation();
+    var hero = new PersonnagePrincipal(scene);  
+    var monInterval = setInterval(
+        function() {
+            if(hero.estCharge){
+                hero.afficher();            
+                createjs.Ticker.addEventListener("tick", rafraichirJeu);            
+            }
+        });
     
+
     var TOUCHE_GAUCHE = 37; 
     var TOUCHE_DROITE = 39;
     var TOUCHE_ESPACE = 32;
     var TOUCHE_CONTROLE = 17;
 
-    function gererTouche(evenement)
+    function gererToucheEnfoncee(evenement)
     {
-        
-     switch(evenement.keyCode)
-            {
-                case TOUCHE_GAUCHE:
-                   
-                    hero.deplacerGauche();
+
+        switch(evenement.keyCode)
+        {
+            case TOUCHE_GAUCHE:
+
+                hero.deplacerGauche();
                 break;
-                case TOUCHE_DROITE:
-                  
-                    hero.deplacerDroite();
+            case TOUCHE_DROITE:
+
+                hero.deplacerDroite();
                 break;
-                case TOUCHE_ESPACE:
-                    
-                    hero.sauter();
+            case TOUCHE_ESPACE:
+
+                hero.sauter();
                 break;
-                case TOUCHE_CONTROLE:
-                    
-                    hero.gobber();
+            case TOUCHE_CONTROLE:
+
+                hero.gobber();
                 break;
-            }
-        
-            scene.update();   
+        }
+
+           
     }
     
-    this.document.onkeydown = gererTouche;
+     function gererToucheRelachee(evenement)
+    {
 
+        switch(evenement.keyCode)
+        {
+            case TOUCHE_GAUCHE:
+
+                hero.metreEnAttente();
+                break;
+            case TOUCHE_DROITE:
+
+                hero.metreEnAttente();
+                break;
+            case TOUCHE_ESPACE:
+
+                hero.metreEnAttente();
+                break;
+            case TOUCHE_CONTROLE:
+
+                hero.metreEnAttente();
+                break;
+        }
+
+           
+    }
     
+    
+
+    this.document.onkeydown = gererToucheEnfoncee;
+    this.document.onkeyup = gererToucheRelachee;
+    
+
+
 })();
--- personnage/PersonnagePrincipal.js
+++ personnage/PersonnagePrincipal.js
@@ -1,59 +1,100 @@
-function PersonnagePrincipal(dessin)
+function PersonnagePrincipal(scene)
 {
-  var personnagePrincipal = this;    
-  var blop = new Image();
-  this.estCharge = false;
-  
-     blop.src = "personnage/blop.png";
-    
-    var spriteBlop = new createjs.SpriteSheet(
-        {
-            images:[blop],
-            frames:{width:120,height:119},
-            animations:
-            {
-                saute:[1,2,3,4,5,6]
-            }
-        }
-    );
-       
-    this.animationBlop = new createjs.Sprite(spriteBlop, "saute");
-       
-    
-        this.jouerAnimation = function(){
-            this.animationBlop.play();
-        }
-        
-  
-   
-   this.deplacerDroite = function()
-   {
-       this.animationBlop.x += 20;
-       
-   }
-   this.deplacerGauche = function()
-   {
-       this.animationBlop.x -= 20
-       
-   }
-   this.sauter = function()
-   {
-       var i = 0;
-       while(i != 10)
-       {
-        if(i < 5)
-            {this.animationBlop.y += 30;}
-        else
-        {this.animationBlop.y -= 30;}
-        i++;   
-       }
+    var personnagePrincipal = this;    
+    var blop = new Image();
+    this.estCharge = false;
+    var animationBlop;
+    var Etat = {
+        enDeplacementDroit:"EN DEPLACEMENT DROIT",
+        enDeplacementgauche:"EN DEPLACEMENT GAUCHE",
+        enSautHaut:"EN SAUT HAUT",
+        enSautBas:"EN SAUT BAS",
+        enAttente:"EN ATTENTE"
+    }
+    var etatCourant;
+    blop.onload = terminerChargement;
+    blop.src = "personnage/blop.png";
 
-       
-   }
-   this.gobber = function()
-   {
-       
-   }
-   
+    function terminerChargement()
+    {
+        var spriteBlop = new createjs.SpriteSheet(
+            {
+                images:[blop],
+                frames:{width:120,height:119, count:6},
+                framerate: 1,
+                animations:
+            {
+                idle:[0,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+            }
+                
+                    
+                
+            }
+        );
+
+        animationIdle = new createjs.Sprite(spriteBlop, "idle");
+        animationSaute = new createjs.Sprite(spriteBlop, "saute");
+        personnagePrincipal.estCharge = true;   
+        etatCourant = Etat.enAttente;
+    }
+
+
+
+    this.afficher = function()
+    {
+        scene.addChild(animationIdle);
+    }
+    this.deplacer = function(deplacement)
+    {
+        switch(etatCourant){
+            case Etat.enDeplacementDroit:
+                animationIdle.x += deplacement;
+                break;
+
+            case Etat.enDeplacementgauche:
+                animationBlop.x -= deplacement;
+                break;
+            case Etat.enAttente:
+                break;
+        }
+
+
+
+    }
+
+
+
+
+    this.deplacerDroite = function()
+    {
+        etatCourant = Etat.enDeplacementDroit;            
+    }
+    this.deplacerGauche = function()
+    {
+        etatCourant = Etat.enDeplacementgauche;
+    }
+    this.metreEnAttente = function()
+    {
+        etatCourant = Etat.enAttente;
+    }
+    this.sauter = function()
+    {
+        var i = 0;
+        while(i != 10)
+        {
+            if(i < 5)
+            {this.animationBlop.y += 30;}
+            else
+            {this.animationBlop.y -= 30;}
+            i++;   
+        }
+
+
+    }
+    this.gobber = function()
+    {
+
+    }
+
 
 }
